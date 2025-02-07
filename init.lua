b456c40e0b0a3c6b3f46428454f5f0075bd53b4a
RioRichard <thanh.huuphungak47@gmail.com>
2025-02-07 08:52:34 +0700

    fix: remove vi-vn spellcheck

--- init.lua
+++ init.lua
@@ -4,5 +4,5 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.opt.spell = true
-vim.opt.spelllang = { "en_us", "vi" }
+vim.opt.spelllang = "en_us"
 require("config.lazy")
